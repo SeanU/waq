6179128c6ad1029eaf213f727f9ba1ad8ed0d8ef
Sean Underwood <flyinghamsterster@gmail.com>
2016-10-04 19:51:00 -0500

    Second (better) crack at OSHA limit scraping

--- scripts/scraping/scraping/spiders/osha.py
+++ scripts/scraping/scraping/spiders/osha.py
@@ -9,13 +9,25 @@ class OshaSpider(scrapy.Spider):
         'https://www.osha.gov/dsg/annotated-pels/tablez-1.html',
     )
 
+
     def parse(self, response):
+        def isSpan(td):
+            return td.xpath('colspan').extract_first() is not None
+
         for row in response.css('#z-1 tbody tr'):
-            cells = row.css('td::text').extract()
-            if len(cells) == 7:
-                yield {
-                    'substance': cells[0],
-                    'cas_no': cells[1],
-                    'pel_ppm': cells[2],
-                    'pel_mg_m3': cells[3]
-                }
+            cells = row.css('td')
+            if len(cells) < 4:
+                continue
+            if any(isSpan(elem) for elem in cells[:4]):
+                # see "Beryllium and beryllium compounds (as Be)"
+                # on table Z-1 for an example of what this is trying
+                # to skip
+                continue
+            yield {
+                'substance': cells[0].xpath('text()').extract_first(),
+                'cas_no': cells[1].xpath('text()').extract_first(),
+                'pel_ppm': cells[2].xpath('text()').extract_first(),
+                'pel_mg_m3': cells[3].xpath('text()').extract_first()
+            }
+
+
