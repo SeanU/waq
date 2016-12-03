c8457764f2bde54325c9507af4f536f1e02bf5c8
ashley-dsci <ashley-dsci@ischool.berkeley.edu>
2016-12-03 18:45:33 -0500

    updating bio cleaning to be more generic

--- scripts/clean-water-bio.py
+++ scripts/clean-water-bio.py
@@ -6,25 +6,46 @@ import pandas as pd
 bio_names =  ['Fecal Coliform', 'Enterococcus', 'Escherichia coli', 
               'Total Coliform']
 
+#list of unit values we are interested in 
+units = ['MPN', 'cfu/100ml', 'MPN/100ml', 'cfu/100ml ']
+
 # list of columns we will need
 cols = ['MonitoringLocationIdentifier', 'CharacteristicName',
         'ActivityStartDate', 'ActivityStartTime/Time',
-        'ResultMeasureValue']
+        'ResultMeasureValue', 'ResultMeasure/MeasureUnitCode']
 
 # open the file for reading, pass it to the CSV dict reader
 # this will return each row as a dictionary where the keys 
 # are the header row
-with open('G:/biologicalresult.csv', 'r') as fp:
+with open('./biologicalresult.csv', 'r') as fp:
     reader = csv.DictReader(fp)
     # iterate over each row, form a sublist of just the columns
     # we are interested in, only if the CharacteristicName is in
     # the bio_names list
-    data = [row[c] for row in reader 
-                   for c in cols
-                   if (row['CharacteristicName'] in bio_names)]
+    data = [[row[c] for c in cols] for row in reader 
+                   if (row['ResultMeasure/MeasureUnitCode'] in units)]
                    
 # pass the data to a dataframe along with the columns
 df = pd.DataFrame(data, columns=cols)
+
+#map poor spellings to bio_names standards
+def name_cleaner(name_string):
+    if re.match('fecal', name_string, re.IGNORECASE):
+        return "Fecal Coliform"
+    if re.match('total col', name_string, re.IGNORECASE):
+        return "Total Coliform"
+    if re.match('enteroc', name_string, re.IGNORECASE):
+        return "Enterococcus"
+    if re.match('escherich|e\.coli|e\. coli', name_string, re.IGNORECASE):
+        return "Escherichia coli"
+    #we drop anything else because there are very few other biologicals measured
+    #and they vary widely
+    return 'DROP'
+  
+#apply the name cleaner fumction to the data and drop any other columns        
+df.CharacteristicName = df.CharacteristicName.apply(name_cleaner)
+df.drop(df[df.CharacteristicName == 'DROP'].index, inplace = True)
+
 
 # define a function to create the Status columns
 def set_status(val_string):
@@ -53,7 +74,7 @@ def set_status(val_string):
 # read in the station data, reset the column names
 station_cols = ['MonitoringLocationIdentifier', 'LatitudeMeasure', 'LongitudeMeasure']
 station_cols_new = ['SiteID', 'Lat', 'Lng']
-df_station = pd.read_csv('G:/station.csv', usecols=station_cols)
+df_station = pd.read_csv('./station.csv', usecols=station_cols)
 df_station.columns = station_cols_new
 # oddly, the station data has duplicates.  so we drop the dupes
 df_station = df_station.drop_duplicates(subset=['SiteID'])
@@ -83,22 +104,4 @@ df_out.drop(df_out.index[df_out.Status=='nan'], inplace=True)
 df_out = df_out.drop_duplicates()
 df_out.reset_index(inplace=True, drop=True)
 # push to file
-df_out.to_csv('G:/biological_cleaned.csv')
-
-
-
-# use this section to write out the wanted lines to a new file
-#with open('G:/biologicalresult.csv', 'rb') as fp1:
-#    reader = csv.reader(fp1)
-#    header = next(reader)
-#    index = header.index('CharacteristicName')
-#    with open('U:/biologicalresults_filtered.csv', 'wb') as fp2:
-#        writer = csv.writer(fp2)
-#        writer.writerow(header)
-#        write_count = 0
-#        for i, row in enumerate(reader):
-#            if row[index] in bio_names:
-#                writer.writerow(row)
-#                write_count += 1
-#            if i % 100000 == 0:
-#                print('Rows read: %d, rows written: %d' %(i, write_count))
+df_out.to_csv('./biological_cleaned.csv')
