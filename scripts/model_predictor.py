1bf3144e4c974eaaeba19938c954bed5fb6df116
ashley-dsci <ashley-dsci@users.noreply.github.com>
2016-12-12 21:23:54 -0500

    tweaking for python 2 compatibility

--- scripts/model_predictor.py
+++ scripts/model_predictor.py
@@ -10,7 +10,7 @@ from sklearn.ensemble import RandomForestClassifier
 # to predict:
 # unpickle the model
 with open('trained_models.pickle', 'rb') as fp:
-    meta_model = dill.load(fp, protocol=2)
+    meta_model = dill.load(fp) #add arg protocol=2 if running in python 3
 
 # call predict function, returns list of lists of predictions
 prediction_list = meta_model.estimated_pollution(lat, lng, time_delta=0)
