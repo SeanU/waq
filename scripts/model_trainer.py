3556fd448106c5d8daf4c24168cdf8d21e1f0d0c
ashley-dsci <ashley-dsci@users.noreply.github.com>
2016-12-12 22:26:28 -0500

    tweak for python2

--- scripts/model_trainer.py
+++ scripts/model_trainer.py
@@ -172,7 +172,7 @@ class MetaModel(object):
 
     def to_pickle(self, file_path):
         with open(file_path, 'wb') as fp:
-            dill.dump(self, fp, protocol=2)
+            dill.dump(self, fp) #include arg protocol=2 if using py3 and want to read in py2
 
 
 # load the data and run the trainers
