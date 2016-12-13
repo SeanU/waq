2bff9336e26e1f49862a81b58a61a3f52315698c
ashley-dsci <ashley-dsci@users.noreply.github.com>
2016-12-12 21:54:30 -0500

    fixed typo

--- scripts/model_predictor.py
+++ scripts/model_predictor.py
@@ -13,4 +13,4 @@ with open('trained_models.pickle', 'rb') as fp:
     meta_model = dill.load(fp) #add arg protocol=2 if running in python 3
 
 # call predict function, returns list of lists of predictions
-prediction_list = meta_model.estimated_pollution(lat, lng, time_delta=0)
+prediction_list = meta_model.estimated_pollution(lat, lng, timedelta=0)
